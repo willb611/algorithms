b99c390d7fc16b0997f254c700f610184bfddb4f
William A. O. Brown <will.b611@gmail.com>
2016-05-30 08:27:48 +0100

    Initial commit adding factorial finder using bigIntegers

---
+++ src/main/java/FactorialFinder.java
@@ -0,0 +1,31 @@
+import java.math.BigInteger;
+
+public class FactorialFinder
+{
+  private BigInteger[] factorials;
+  private final int max;
+  public FactorialFinder(int max) {
+    this.max = max;
+    factorials = new BigInteger[max+1];
+    factorials[0]=BigInteger.ONE;
+  }
+
+  public BigInteger findFactorialOf(int numberToFindFactorialOf)
+  {
+    if (numberToFindFactorialOf == 0) {
+      return BigInteger.ONE;
+    } else if (numberToFindFactorialOf < 0) {
+      throw new IllegalArgumentException("Cannot find the factrial of a negative number");
+    }
+    int b = numberToFindFactorialOf;
+    while (factorials[b]==null){
+      b = b >>2;
+    }
+    b++;
+    for (; b <= numberToFindFactorialOf; b++)
+    {
+      factorials[b] = factorials[b-1].multiply(new BigInteger(Integer.toString(b)));
+    }
+    return factorials[numberToFindFactorialOf];
+  }
+}
