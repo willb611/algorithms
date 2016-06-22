17f34b0b5d5e3a1982794e03e68993b70da4bd7e
William A. O. Brown <will.b611@gmail.com>
2016-06-22 21:37:30 +0100

    Fib: cleanup if condition

--- src/main/java/algorithms/sequences/MatricesFibonacciFinder.java
+++ src/main/java/algorithms/sequences/MatricesFibonacciFinder.java
@@ -33,8 +33,12 @@ public class MatricesFibonacciFinder implements FibonacciFinder {
   private static int[][] matrixMul(int[][] A, int[][] B) {
     checkNotNull(A);
     checkNotNull(B);
-    if (A.length != B.length || A.length != 2
-        || A[0].length != 2 || B[0].length != 2) {
+    if (A.length != B.length
+        || A.length != 2
+        || A[0].length != A[1].length
+        || B[0].length != B[1].length
+        || A[0].length != 2
+        || B[0].length != 2) {
       throw new IllegalArgumentException("Must provide two 2x2 matrices to multiply");
     }
     int[][] resultingMatrix = new int[2][2];
