900478ed6b3ff6dc3f502707251b4ec8ab9cfc9e
William A. O. Brown <will.b611@gmail.com>
2016-07-24 18:13:12 +0100

    Convert MatricesFibonacciFinder to use BigInteger properly. Retain int version in another file

--- src/main/java/algorithms/sequences/IntMatricesFibonacciFinder.java
+++ src/main/java/algorithms/sequences/IntMatricesFibonacciFinder.java
@@ -1,37 +1,32 @@
 package algorithms.sequences;
 
-import java.math.BigInteger;
-
 import static internal.Preconditions.checkNotNull;
-import static java.math.BigInteger.ONE;
-import static java.math.BigInteger.ZERO;
 
-public class MatricesFibonacciFinder implements FibonacciFinder {
+public class IntMatricesFibonacciFinder {
 
-  @Override
-  public BigInteger findNthTermInSequence(int n) {
+  public int findNthTermInSequence(int n) {
     if (n <= 2) {
-      return ONE;
+      return 1;
     }
-    final BigInteger[][] T = new BigInteger[2][2];
-    T[0] = new BigInteger[]{ONE, ONE};
-    T[1] = new BigInteger[]{ONE, ZERO};
+    final int[][] T = new int[2][2];
+    T[0] = new int[]{1, 1};
+    T[1] = new int[]{1, 0};
     return raiseMatrixToThePower(T, n - 1)[0][0];
   }
 
-  private static BigInteger[][] raiseMatrixToThePower(BigInteger[][] matrix, int power) {
+  private static int[][] raiseMatrixToThePower(int[][] matrix, int power) {
     checkNotNull(matrix);
     if (power == 1) {
       return matrix;
     } else if (power % 2 == 1) {
       return matrixMul(matrix, raiseMatrixToThePower(matrix, power - 1));
     } else {
-      BigInteger[][] matrixRaisedToHalfPow = raiseMatrixToThePower(matrix, power / 2);
+      int[][] matrixRaisedToHalfPow = raiseMatrixToThePower(matrix, power / 2);
       return matrixMul(matrixRaisedToHalfPow, matrixRaisedToHalfPow);
     }
   }
 
-  private static BigInteger[][] matrixMul(BigInteger[][] A, BigInteger[][] B) {
+  private static int[][] matrixMul(int[][] A, int[][] B) {
     checkNotNull(A);
     checkNotNull(B);
     if (A.length != B.length
@@ -42,16 +37,15 @@ public class MatricesFibonacciFinder implements FibonacciFinder {
         || B[0].length != 2) {
       throw new IllegalArgumentException("Must provide two 2x2 matrices to multiply");
     }
-    BigInteger[][] resultingMatrix = new BigInteger[2][2];
+    int[][] resultingMatrix = new int[2][2];
     for (int row = 0; row < A.length; row++) {
       for (int col = 0; col < A[0].length; col++) {
         // calculate this thing.
-        BigInteger result = ZERO;
+        int res = 0;
         for (int i = 0; i < 2; i++) {
-          BigInteger intermediate = A[row][i].multiply(B[i][col]);
-          result = result.add(intermediate);
+          res += A[row][i] * B[i][col];
         }
-        resultingMatrix[row][col] = result;
+        resultingMatrix[row][col] = res;
       }
     }
     return resultingMatrix;
