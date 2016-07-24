eb25ad34617a6157fb72831cfe2c27baa41361e0
William A. O. Brown <will.b611@gmail.com>
2016-07-24 18:31:23 +0100

    Add slf4j logging library, and add some log statements

--- src/main/java/algorithms/sequences/MatricesFibonacciFinder.java
+++ src/main/java/algorithms/sequences/MatricesFibonacciFinder.java
@@ -1,5 +1,8 @@
 package algorithms.sequences;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.math.BigInteger;
 
 import static internal.Preconditions.checkNotNull;
@@ -7,19 +10,24 @@ import static java.math.BigInteger.ONE;
 import static java.math.BigInteger.ZERO;
 
 public class MatricesFibonacciFinder implements FibonacciFinder {
+  private Logger logger = LoggerFactory.getLogger(MatricesFibonacciFinder.class);
 
   @Override
   public BigInteger findNthTermInSequence(int n) {
+    logger.debug("[findNthTermInSequence] Given n: {}");
     if (n <= 2) {
       return ONE;
     }
     final BigInteger[][] T = new BigInteger[2][2];
     T[0] = new BigInteger[]{ONE, ONE};
     T[1] = new BigInteger[]{ONE, ZERO};
-    return raiseMatrixToThePower(T, n - 1)[0][0];
+    BigInteger result = raiseMatrixToThePower(T, n - 1)[0][0];
+    logger.debug("[findNthTermInSequence] For n: {}, returning result: {}", n, result);
+    return result;
   }
 
-  private static BigInteger[][] raiseMatrixToThePower(BigInteger[][] matrix, int power) {
+  private BigInteger[][] raiseMatrixToThePower(BigInteger[][] matrix, int power) {
+    logger.debug("[raiseMatrixToThePower] Given power: {}", power);
     checkNotNull(matrix);
     if (power == 1) {
       return matrix;
@@ -31,7 +39,7 @@ public class MatricesFibonacciFinder implements FibonacciFinder {
     }
   }
 
-  private static BigInteger[][] matrixMul(BigInteger[][] A, BigInteger[][] B) {
+  private BigInteger[][] matrixMul(BigInteger[][] A, BigInteger[][] B) {
     checkNotNull(A);
     checkNotNull(B);
     if (A.length != B.length
@@ -50,6 +58,7 @@ public class MatricesFibonacciFinder implements FibonacciFinder {
         for (int i = 0; i < 2; i++) {
           BigInteger intermediate = A[row][i].multiply(B[i][col]);
           result = result.add(intermediate);
+          logger.trace("[matrixMul] Got intermediate: {}, and result: {}", intermediate, result);
         }
         resultingMatrix[row][col] = result;
       }
